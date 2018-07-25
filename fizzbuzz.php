8b351f25f8105a0f6a2a29afa4e0a9aa581cdf7b
indoctrine <indoctrine@users.noreply.github.com>
2018-07-25 22:01:59 +0930

    Moving repos around locally

---
+++ fizzbuzz.php
@@ -0,0 +1,14 @@
+<?php
+  for($i=1; $i<=100; $i++){
+    if($i % 3 == 0){
+      echo "Fizz";
+    }
+    if($i % 5 == 0){
+      echo "Buzz";
+    }
+    if($i % 5 != 0 && $i % 3 !=0){
+      echo $i;
+    }
+    echo "<br>";
+  }
+?>
